7030d531d74aae02898738d1061603033ae319d0
Ryan McGuire <ryan@enigmacurry.com>
2018-04-16 08:11:49 -0400

    Redirect http to https

--- terraform/main.tf
+++ terraform/main.tf
@@ -126,7 +126,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       }
     }
 
-    viewer_protocol_policy = "https-only"
+    viewer_protocol_policy = "redirect-to-https"
     min_ttl = 0
     default_ttl = 3600
     max_ttl = 86400
